58ce00eb83fee94ec5354df5cce354e8e439ce00
joedolson <joedolson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-01 18:27:47 +0000

    Commit mods to mc-list.js (not yet functional.)
    git-svn-id: https://plugins.svn.wordpress.org/my-calendar/trunk@1257626 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/mc-list.js
+++ js/mc-list.js
@@ -3,15 +3,15 @@
     $(function () {
         $("li.mc-events").children().not(".event-date").hide();
         $("li.current-day").children().show();
-        $(document).on("click", ".event-date",
+        $(document).on("click", ".event-date a",
             function (e) {
                 e.preventDefault();
-                $(this).parent().children().not(".event-date").toggle().attr("tabindex", "-1").focus();
-                var visible = $(this).parent().find(".vevent").is(":visible");
+                $(this).closest( '.mc-events' ).find( '.vevent' ).toggle().attr("tabindex", "-1").focus();
+                var visible = $(this).closest( '.mc-events' ).find(".vevent").is(":visible");
                 if (visible) {
-                    $(this).parent().find(".vevent").attr("aria-expanded", "true");
+                    $(this).closest( '.mc-events' ).find(".vevent").attr("aria-expanded", "true");
                 } else {
-                    $(this).parent().find(".vevent").attr("aria-expanded", "false");
+                    $(this).closest( '.mc-events' ).find(".vevent").attr("aria-expanded", "false");
                 }
             });
     });
